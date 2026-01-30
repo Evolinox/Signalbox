f8d3764075c8ad42f4abad7fa61e3bda0c592eaf
Pascal <pascal@jedicke.me>
2026-01-30 11:34:12 +0100

    feat(docs): prepare structure

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,59 @@
+import { defineConfig } from 'vitepress'
+import { version } from '../../package.json'
+import { linkChangelog, linkRelease, socialDiscord, socialGit } from "./meta";
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  srcDir: "content",
+  
+  title: "Signalbox",
+  description: "Modern way to control your Modelrailroad",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Docs', link: '/docs/overview/introduction' },
+      { text: 'Showcase', link: '/showcase' },
+      {
+        text: `v${version}`,
+        items: [
+          { text: 'Changelog', link: linkChangelog },
+          { text: 'Release', link: linkRelease }
+        ]
+      }
+    ],
+
+    sidebar: [
+      {
+        text: 'Overview',
+        icon: 'lucide:rocket',
+        items: [
+          { text: 'Introduction', link: '/docs/overview/introduction' },
+          { text: 'Changelog', link: '/docs/overview/changelog' }
+        ]
+      },
+      {
+        text: 'Control Box',
+        icon: 'lucide:box',
+        items: [
+          { text: 'Introduction', link: '/docs/control-box/introduction' }
+        ]
+      },
+      {
+        text: 'Remote Box',
+        icon: 'lucide:box',
+        items: [
+          { text: 'Introduction', link: '/docs/remote-box/introduction' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: socialGit },
+      { icon: 'discord', link: socialDiscord }
+    ],
+
+    search: {
+      provider: 'local',
+    }
+  }
+})
---
+++ docs/.vitepress/meta.ts
@@ -0,0 +1,7 @@
+export const version = '1.0.0'
+
+export const linkChangelog = '/docs/overview/changelog'
+export const linkRelease = 'https://github.com/evolinox/signal-box/releases'
+
+export const socialGit = 'https://github.com/evolinox/signal-box'
+export const socialDiscord = ''
